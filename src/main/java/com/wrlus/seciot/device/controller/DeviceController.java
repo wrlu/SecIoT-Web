8db4683e0615a187c738078907b6483e951a9cac
wrlu <wrlus@outlook.com>
2019-05-14 15:52:10 +0800

    DeviceController Signed-off-by: wrlu <wrlus@outlook.com>

--- src/main/java/com/wrlus/seciot/device/controller/DeviceController.java
+++ src/main/java/com/wrlus/seciot/device/controller/DeviceController.java
@@ -1,10 +1,32 @@
 package com.wrlus.seciot.device.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wrlus.seciot.device.model.DeviceDao;
 
 @Controller
 @RequestMapping("/device")
 public class DeviceController {
-
+	
+	@ResponseBody
+	@RequestMapping("/add")
+	public Map<String, Object> add(DeviceDao deviceDao, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> data = new HashMap<>();
+		return data;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update")
+	public Map<String, Object> update(DeviceDao deviceDao, HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> data = new HashMap<>();
+		return data;
+	}
 }
